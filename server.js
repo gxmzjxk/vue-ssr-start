fc79167db3297ce530e4001f42075b502c846b64
蒋小昆 <xiaokunjiang@sohu-inc.com>
2018-12-24 21:10:23 +0800

    Feat: add createRenderer template

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 const Vue = require('vue')
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+const createRenderer = require('vue-server-renderer').createRenderer;
+
+// 使用模板, 模板还支持简单的插值。语法使用 Vue 语法
+const renderer = createRenderer({
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
+})
+
 
 server.get('*', (req, res) => {
   const app = new Vue({
     data: {
       url: req.url
     },
-    template: `<div>访问的 URL 是： {{ url }}</div>`
+    template: `<div>这是通过Template 方式访问，URL 是： {{ url }}</div>`
   })
+  const context = {
+    title: 'Hello, Vue SSR',
+    meta: `
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    `
+  }
+  // renderToString(vm: Vue, context: object, callback: RenderCallback): void;
 
-  renderer.renderToString(app, (err, html) => {
+  renderer.renderToString(app, context, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
     }
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-            <title>Hello</title>
-            <meta charset="UTF-8">
-        </head>
-        <body>${html}</body>
-      </html>
-    `)
+    res.end(html)
   })
 })
 
